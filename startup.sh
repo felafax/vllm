fe186e492d5cd671de8ec84b90a586eb295c79f8
Nikhil Sonti <nikhilsv92@gmail.com>
2024-09-03 15:47:03 -0700

    using cloud storage bucket also as env variable

--- startup.sh
+++ startup.sh
@@ -7,9 +7,9 @@ mkdir -p /workspace/felafax-storage
 # mount gcsfuse
 if [ "$MOUNT_GCSFUSE" = "1" ]; then
   if [ "$UID" != "0" ]; then
-    gcsfuse --implicit-dirs --only-dir "$UID" felafax-storage "/workspace/felafax-storage/"
+    gcsfuse --implicit-dirs --only-dir "$UID" $CLOUD_STORAGE_BUCKET "/workspace/felafax-storage/"
   else
-    gcsfuse --implicit-dirs felafax-storage "/workspace/felafax-storage/"
+    gcsfuse --implicit-dirs $CLOUD_STORAGE_BUCKET "/workspace/felafax-storage/"
   fi
 fi
 
