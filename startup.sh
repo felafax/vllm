1d099627e09da3fb5608533067ab06952d6adf85
Nikhil Sonti <nikhilsv92@gmail.com>
2024-09-04 17:33:36 -0700

    more storage path stuff trying

--- startup.sh
+++ startup.sh
@@ -1,53 +1,48 @@
 #!/bin/bash
 set -eo pipefail
 
+# Define the storage directory as a variable
+STORAGE_DIR="/workspace/storage"
+
 # create dir
-mkdir -p /workspace/felafax-storage
+mkdir -p "$STORAGE_DIR"
 
 # mount gcsfuse
-# if [ "$MOUNT_GCSFUSE" = "1" ]; then
-#   if [ "$UID" != "0" ]; then
-#     echo "Mounting only directory $UID"
-#     gcsfuse --implicit-dirs --only-dir "$UID" $CLOUD_STORAGE_BUCKET "/workspace/felafax-storage/"
-#   else
-#     echo "Mounting entire bucket"
-#     gcsfuse --implicit-dirs $CLOUD_STORAGE_BUCKET "/workspace/felafax-storage/"
-#   fi
-# fi
-#
 echo "Mounting entire bucket"
-gcsfuse --implicit-dirs $CLOUD_STORAGE_BUCKET "/workspace/felafax-storage/"
+sudo mount -t gcsfuse -o implicit_dirs $CLOUD_STORAGE_BUCKET "$STORAGE_DIR"
 
 # Check if the mounted directory is empty
-if [ -z "$(ls -A /workspace/felafax-storage/)" ]; then
+if [ -z "$(ls -A "$STORAGE_DIR")" ]; then
   echo "Mounted directory is empty. Retrying mount..."
-  fusermount -u "/workspace/felafax-storage/"
-  gcsfuse --implicit-dirs $CLOUD_STORAGE_BUCKET "/workspace/felafax-storage/"
+  fusermount -u "$STORAGE_DIR"
+
+  # retry
+  STORAGE_DIR="/workspace/storage-2"
+  mkdir -p "$STORAGE_DIR"
+
+  gcsfuse --implicit-dirs $CLOUD_STORAGE_BUCKET "$STORAGE_DIR"
 
   # Check again after retry
-  if [ -z "$(ls -A /workspace/felafax-storage/)" ]; then
+  if [ -z "$(ls -A "$STORAGE_DIR")" ]; then
     echo "Error: Failed to mount the bucket. Directory is still empty after retry." >&2
     exit 1
   fi
 fi
 
 # run vLLM from local model
-if [ -n "$MODEL_PATH" ] && [ -d "/workspace/felafax-storage/$MODEL_PATH" ]; then
+if [ -n "$MODEL_PATH" ] && [ -d "$STORAGE_DIR/$MODEL_PATH" ]; then
   # local model path
   target_dir="/workspace/$MODEL_PATH"
   mkdir -p "$target_dir"
-
   # copy the model locally from gcsfuse
-  time cp -R "/workspace/felafax-storage/$MODEL_PATH"/* "$target_dir"
+  time cp -R "$STORAGE_DIR/$MODEL_PATH"/* "$target_dir"
   echo "Using local model from $MODEL_PATH"
-
   # start vllm
   echo "Starting vLLM server on $VLLM_PORT and using model $target_dir"
   CMD="python3 -m vllm.entrypoints.openai.api_server --model $target_dir --port $VLLM_PORT --dtype auto"
 elif [ -n "$HF_PATH" ]; then
   echo "Using Hugging Face model from $HF_PATH"
   export HUGGING_FACE_HUB_TOKEN=$HF_TOKEN
-
   # start vllm
   echo "Starting vLLM server on $VLLM_PORT and using model $HF_PATH"
   CMD="python3 -m vllm.entrypoints.openai.api_server $HF_PATH --port $VLLM_PORT --dtype auto"
