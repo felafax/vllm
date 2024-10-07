d49d8e99f6ab15c2acadd66258f4831c2b710747
Nikhil Sonti <nikhilsv92@gmail.com>
2024-10-07 11:00:03 +0530

    new docker push script

---
+++ build_and_push_docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set the Docker image name and tag
+IMAGE_NAME="gcr.io/felafax-training/vllm"
+IMAGE_TAG="latest_v3"
+
+# Build the Docker image
+echo "Building Docker image... ${IMAGE_NAME}:${IMAGE_TAG}"
+sudo docker build -f Dockerfile.felafax.tpu -t ${IMAGE_NAME}:${IMAGE_TAG} .
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker image built successfully."
+    
+    # Push the Docker image
+    echo "Pushing Docker image to GCR..."
+    sudo -E docker push ${IMAGE_NAME}:${IMAGE_TAG}
+    
+    if [ $? -eq 0 ]; then
+        echo "Docker image pushed successfully."
+    else
+        echo "Failed to push Docker image."
+        exit 1
+    fi
+else
+    echo "Docker build failed."
+    exit 1
+fi
+
+echo "Script completed successfully."
