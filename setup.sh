4515d500cf1d65a48ddf2f10a21a165d01fb6c3a
Nikhil Sonti <nikhilsv92@gmail.com>
2024-10-09 12:07:12 +0530

    set model path

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ if [ -n "$GCS_MODEL_PATH" ]; then
   echo "Copying model files to persistent disk..."
   mkdir -p /mnt/persistent-disk/model
   gsutil -m cp -r $GCS_MODEL_PATH/* /mnt/persistent-disk/model
+  MODEL="/mnt/persistent-disk/model"
 elif [ -n "$HF_PATH" ]; then
   MODEL="$HF_PATH"
 fi
