60aed30f49935aaac5abbc6ea71ea9f21d1255b8
Nikhil Sonti <nikhilsv92@gmail.com>
2024-10-10 12:58:06 +0530

    handle path

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ MODEL=""
 if [ -n "$GCS_MODEL_PATH" ]; then
   echo "Copying model files to persistent disk..."
   mkdir -p /mnt/persistent-disk/model
-  gsutil -m cp -r $GCS_MODEL_PATH/* /mnt/persistent-disk/model
+  gsutil -m cp -r "${GCS_MODEL_PATH%/}"/* /mnt/persistent-disk/model
   MODEL="/mnt/persistent-disk/model"
 elif [ -n "$HF_PATH" ]; then
   MODEL="$HF_PATH"
 fi
 
-echo "vllm serve..."
+echo "vllm serve... {model: $MODEL}"
 exec vllm serve $MODEL --dtype auto
