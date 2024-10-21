77a815d74e4b5ca654b7ba5a18dc3a3d7e5f7fcf
Nikhil Sonti <nikhilsv92@gmail.com>
2024-10-21 09:18:35 +0100

    fixing the vllm start script

--- setup.sh
+++ setup.sh
@@ -3,12 +3,16 @@ MODEL=""
 if [ -n "$GCS_MODEL_PATH" ]; then
   echo "Copying model files to persistent disk..."
   mkdir -p /mnt/persistent-disk/model
+
   gsutil -m cp -r "${GCS_MODEL_PATH%/}"/* /mnt/persistent-disk/model
   MODEL="/mnt/persistent-disk/model"
+
+  echo "vllm serve... {model: $MODEL}"
+  exec vllm serve $MODEL --tokenizer "/mnt/persistent-disk/model" --enforce-eager
+
 elif [ -n "$HF_PATH" ]; then
   MODEL="$HF_PATH"
-fi
 
-echo "vllm serve... {model: $MODEL}"
-# exec vllm serve $MODEL --dtype auto
-exec vllm serve $MODEL --tokenizer "/mnt/persistent-disk/model" --enforce-eager
+  echo "vllm serve... {model: $MODEL}"
+  exec vllm serve $MODEL  --enforce-eager
+fi
